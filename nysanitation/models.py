9f3e20a58edbde0770addd65d2eb6a7f32410f92
Gabe Gordon <gabegordon123@gmail.com>
2018-12-09 17:46:27 -0500

    Fix imports

--- nysanitation/models.py
+++ nysanitation/models.py
@@ -1,11 +1,13 @@
+import uuid
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class user(models.Model):
     name = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    resturants = models.ArrayField(models.CharField(max_length=100))
+    resturants = ArrayField(models.CharField(max_length=100))
 
 class resturants(models.Model):
     ID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -13,6 +15,6 @@ class resturants(models.Model):
     Zipcode  = models.CharField(max_length=100)
     Address  = models.CharField(max_length=100)
     Sanitation = models.DecimalField()
-    Sickness = models.IntergerField()
+    Sickness = models.IntegerField()
 
 # Create your models here.
